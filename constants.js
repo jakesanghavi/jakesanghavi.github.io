5e3eb5bd17d4690328d33f796f8a7cabdc0ea3f9
jakesanghavi <jake.sanghavi@gmail.com>
2026-05-06 10:15:53 -0400

    routes

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 // For Prod
-// export const ORIGINS = ['https://jakesanghavi.netlify.app', 'https://jakesanghavi.com'];
-// export const ROUTE = 'https://jakesanghavi-github-io.onrender.com';
+export const ORIGINS = ['https://jakesanghavi.netlify.app', 'https://jakesanghavi.com'];
+export const ROUTE = 'https://jakesanghavi-github-io.onrender.com';
 
 // // // For Development
-export const ORIGINS = 'http://localhost:5173';
-export const ROUTE = 'http://localhost:4000';
+// export const ORIGINS = 'http://localhost:5173';
+// export const ROUTE = 'http://localhost:4000';
